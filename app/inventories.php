70a40010debe0d6192401ed72bd9b65efd1e6ae4
KipsangA <40466099+KipsangA@users.noreply.github.com>
2021-01-27 09:52:01 +0300

    general

--- app/inventories.php
+++ app/inventories.php
@@ -51,7 +51,52 @@
 					<input type="text" name="search_invent" placeholder="Search Inventory">
 					<button class="btn btn-info" type="submit" name="search_invent_btn">Search</button>
 				</form> -->
-      <a class="btn btn-success" href="report.php">Report</a>
+      <!-- <a class="btn btn-success" href="report.php">Report</a> -->
+      <form method="post">  
+                          <input type="submit" name="Print" class="btn btn-primary" value="Print" />  
+                     </form>
+      <?php
+      if(isset($_POST["Print"]))  
+      {  
+           require_once('TCPDF/tcpdf.php');  
+           $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+           $obj_pdf->SetCreator(PDF_CREATOR);  
+           $obj_pdf->SetTitle("List of Inventories");  
+           $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+           $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
+           $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+           $obj_pdf->SetDefaultMonospacedFont('helvetica');  
+           $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
+           $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);  
+           $obj_pdf->setPrintHeader(false);  
+           $obj_pdf->setPrintFooter(false);  
+           $obj_pdf->SetAutoPageBreak(TRUE, 10);  
+           $obj_pdf->SetFont('helvetica', '', 11);  
+           $obj_pdf->AddPage();  
+           $content = '';  
+           $content .= '
+             <img src="TCPDF/images/logo.png"></img>
+           <h4 align="center" color="black">Inventory Report</h4><br /> 
+           <table border="1" cellspacing="0" cellpadding="3">  
+                <tr>  
+                <th width="12%">Description</th>
+                <th width="7%">Brand</th>  
+                <th width="15%">Model</th>
+                <th width="15%">Serial</th>
+                <th width="10%">Location</th>      
+                <th width="15%">Department</th> 
+                <th width="10%">User</th> 
+                <th width="11%">Condition</th>   
+                                       
+                </tr>  
+           ';  
+           $content .= fetch_data();  
+           $content .= '</table>';  
+           $obj_pdf->writeHTML($content);  
+           $obj_pdf->Output('Inventory report.pdf', 'I');  
+      }  
+      ?>  
+      ?>
      </div>
      <div class="col-sm-6">
      </div>
