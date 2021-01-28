56659d4c2e4c9158f86f8991c33fcf2914b888b6
KipsangA <40466099+KipsangA@users.noreply.github.com>
2021-01-28 14:00:36 +0300

    general

--- app/partials/editinvent.php
+++ app/partials/editinvent.php
@@ -1,11 +1,9 @@
 <?php 
                if (isset($_GET['id'])) {
                	$get_id =$_GET['id'];
-
-
-               	$get_post ="select *from registrations_invent where id='$get_id'";
-               	$run_post =mysqli_query($con, $get_post);
-               	$row = mysqli_fetch_array($run_post);
+               	$get_inv ="select *from registrations_invent where id='$get_id'";
+               	$run_inv =mysqli_query($con, $get_inv);
+               	$row = mysqli_fetch_array($run_inv);
 
                	$errMsg = '';
 			// Get data from FROM
@@ -33,21 +31,21 @@
 		  	 	<div class="col-md-4">
 			  	  <div class="form-group">
 				    <label for="description">Description</label>
-				    <input type="text" class="form-control" id="description" placeholder=" description" name="description" <?php echo $description; ?> required>
+				    <input type="text" class="form-control" id="description" name="description" <?php echo $description; ?> required>
 				  </div>
 				 </div>
 
 				<div class="col-md-4">
 				  <div class="form-group">
 				    <label for="brand">Brand</label>
-				    <input type="text" class="form-control" id="brand" title="brand" placeholder="brand" name="brand" <?php echo $brand; ?> required>
+				    <input type="text" class="form-control" id="brand" title="brand" name="brand" <?php echo $brand; ?> required>
 				  </div>
 				 </div>
 
 				<div class="col-md-4">
 				  <div class="form-group">
 				    <label for="model">Model</label>
-				    <input type="text" class="form-control" id="model" title="model" placeholder="model" name="model" <?php echo $model; ?>>
+				    <input type="text" class="form-control" id="model" title="model" name="model" <?php echo $model; ?>>
 				  </div>
 				</div>
 			</div>
@@ -56,7 +54,7 @@
 		  	 	<div class="col-md-4">
 				  <div class="form-group">
 				    <label for="serial_no">Serial Number</label>
-				    <input type="serial_no" class="form-control" id="serial_no" placeholder="serial number" name="serial_no" <?php echo $serial_no; ?> required>
+				    <input type="serial_no" class="form-control" id="serial_no" name="serial_no" <?php echo $serial_no; ?> required>
 				  </div>
 				 </div>
 
@@ -65,7 +63,7 @@
 				    <label for="location">Location</label>
 					<select type="text" class="form-control" id="location" name="location" required>
                     
-                          <option value="$location"><?php echo $location; ?></option>  
+                          <option><?php echo $location; ?></option>  
 					      <option value="PSM">PSM</option>
 					      <option value="FINANCE">FINANCE</option>
 						  <option value="TOURISM">TOURISM</option>
@@ -79,7 +77,7 @@
 				 <div class="col-md-4">
 				  <div class="form-group">
 				    <label for="department">Department</label>
-				    <input type="text" class="form-control" id="department" placeholder="department" name="department" <?php echo $department; ?> required>
+				    <input type="text" class="form-control" id="department" name="department" <?php echo $department; ?> required>
 				  </div>
 				 </div>
 			</div>
@@ -88,13 +86,13 @@
 				<div class="col-md-4">
 			  		<div class="form-group">
 			    	<label for="assigned_to">Assigned To</label>
-			    	<input type="assigned_to" class="form-control" id="assigned_to" placeholder="assigned to" name="assigned_to" <?php echo $assigned_to; ?> required>
+			    	<input type="assigned_to" class="form-control" id="assigned_to" name="assigned_to" <?php echo $assigned_to; ?> required>
 			  		</div>
 
 					 <div class="form-group">
 					    <label for="status">Condition</label>
 					    <select type="text" class="form-control" id="status" name="status" required>
-                        <option value="$status"><?php echo $status; ?></option>
+                        <option><?php echo $status; ?></option>
 					      <option value="Working">Working</option>
 					      <option value="Not Working">Not Working</option>
 					    </select>
@@ -103,7 +101,7 @@
 			 		 
 			</div>
 			  </div>			
-			  <input type="submit" name="update" value="Update Post" class="btn btn-info"/>
+			  <input type="submit" name="update" value="Update" class="btn btn-info"/>
 			</form>	
             <?php 
 
@@ -123,8 +121,8 @@
                 $run_update = mysqli_query($con, $update_post);
 
                 if ($run_update) {
-                	echo "<script>alert('A post has been updated')</script>";
-    				echo "<script>window.open('home.php', '_self')</script>";
+                	echo "<script>alert('An Inventory has been updated')</script>";
+    				echo "<script>window.open('update.php', '_self')</script>";
                 }
               } 
 			  ?>
